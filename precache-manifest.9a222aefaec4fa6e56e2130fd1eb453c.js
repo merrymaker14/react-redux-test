591c22b1248d61f4560d796a4fa29d7f3db13dea
merrymaker14 <tmusharapov@bk.ru>
2019-09-03 09:54:52 +0300

    Updates

---
+++ precache-manifest.9a222aefaec4fa6e56e2130fd1eb453c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-redux-test/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "acceb0dd77154dcfd4e8",
+    "url": "/react-redux-test/static/js/runtime~main.acceb0dd.js"
+  },
+  {
+    "revision": "6dc81927b3fd0012bdac",
+    "url": "/react-redux-test/static/js/main.6dc81927.chunk.js"
+  },
+  {
+    "revision": "6dbcf7f67df53f046841",
+    "url": "/react-redux-test/static/js/1.6dbcf7f6.chunk.js"
+  },
+  {
+    "revision": "bbb4e0545eddefd7170079eff051ab44",
+    "url": "/react-redux-test/index.html"
+  }
+];
